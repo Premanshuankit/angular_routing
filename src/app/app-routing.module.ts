7f718c2371c998501ef6dfe3d041d12726516cb0
Premanshu Ankit <ankit.premanshu@gmail.com>
2019-10-12 01:30:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomesComponent } from './components/homes/homes.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomesComponent },
+  { path: 'home', component: HomesComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'user', component: UsersComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { HomesComponent } from './components/homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    SidebarComponent,
+    PagenotfoundComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule,
